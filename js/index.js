63af6d862e2bfb3c95dea80a6516f782276e43a3
s7k11 <bpandey9876@gmail.com>
2019-02-13 23:48:57 +0530

    changed

--- js/index.js
+++ js/index.js
@@ -1,4 +1,26 @@
-
-		$('load').click(function(){
-			console.log('woking');
+$(document).ready(function(){
+		$(".btn-align").click(function(){
+			$('.show1').css('opacity',1);
+			$('.show2').css('opacity',1);
+			$('.show3').css('opacity',1);
+			$('.show4').css('opacity',1);
+			$('.show5').css('opacity',1);
+			$('.show6').css('opacity',1);
+			$('.show7').css('opacity',1);
+			$('.btn-minus').css('opacity',1);
+			$('.btn-align').css('opacity',0);
 		});
+	$(".btn-minus").click(function(){
+			$('.show1').css('opacity',0);
+			$('.show2').css('opacity',0);
+			$('.show3').css('opacity',0);
+			$('.show4').css('opacity',0);
+			$('.show5').css('opacity',0);
+			$('.show6').css('opacity',0);
+			$('.show7').css('opacity',0);
+			$('.btn-minus').css('opacity',0);
+			$('.btn-align').css('opacity',1);
+		});
+			
+});
+		
